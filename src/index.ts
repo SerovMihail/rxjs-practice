2b60b1edce28217e07f36a61b5ecde172115421f
SerovMikhail <serov.mikhail.job@gmail.com>
2019-09-08 20:49:54 +0200

    the simplest example that will help to understand high order mapping

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import { ajax } from "rxjs/ajax";
-import {
-  fromEvent,
-  pipe,
-  forkJoin,
-  of,
-  Subject,
-  Observable,
-  interval
-} from "rxjs";
+import { fromEvent, pipe, forkJoin, of, Subject, Observable, concat } from "rxjs";
 import {
   mergeMap,
   combineAll,
@@ -15,19 +7,16 @@ import {
   switchMap,
   takeUntil,
   map,
-  take
+  exhaustMap
 } from "rxjs/operators";
 
-// const API_URL = `https://medicalreachapidev.azurewebsites.net`;
+const API_URL = `https://medicalreachapidev.azurewebsites.net`;
 
 // const getCountries = ajax.getJSON(`${API_URL}/api/info/country`);
-// const getAllLists = ajax.getJSON(`${API_URL}//\api/info/all-lists`);
+// const getAllLists = ajax.getJSON(`${API_URL}/api/info/all-lists`);
 
-// forkJoin({ lists: getAllLists, conuntries: getCountries })
-//   .pipe(
-//     switchMap((params: any) => {
-//       return interval(500)
-//     }),
+// concat(getAllLists, getCountries)
+//   .pipe(    
 //     concatMap(mergeData => {
 //       console.log("mergeData:", mergeData);
 //       return of({
@@ -40,36 +29,8 @@ import {
 //     console.log(_);
 //   });
 
-// Just outputs the number of seconds since the start to visualize the time flow
-// Not relevant to the example
-interval(1000)
-  .pipe(take(5))
-  .subscribe(s => console.log(`⏰ ${s} sec`));
-
-// Turns values from outer Observable into inner Observables
-function getInnerObservable(outerParam: any) {
-  return interval(1000).pipe(
-    take(3),
-    map(n => `Inner item ${n} for ${outerParam}`)
-  );
-}
-
-const outerObservable = interval(1000).pipe(
-  take(3),
-  map(n => `Outer item ${n}`)
-);
-
-// Uncomment for MergeMap
-// outerObservable
-//   .pipe(mergeMap(outerParam => getInnerObservable(outerParam)))
-//   .subscribe(x => console.log(x));
-
-// Uncomment for ConcatMap
-// outerObservable
-//   .pipe(concatMap(outerParam => getInnerObservable(outerParam)))
-//   .subscribe(x => console.log(x));
-
-// Uncomment for SwitchMap
-outerObservable
-  .pipe(switchMap(outerParam => getInnerObservable(outerParam)))
-  .subscribe(x => console.log(x));
+fromEvent(document, 'click')
+    .pipe(
+        switchMap(() => ajax.getJSON(`${API_URL}/api/info/all-lists`))
+    )
+    .subscribe();
