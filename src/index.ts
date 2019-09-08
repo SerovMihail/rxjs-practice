cf373b086e608ac76900649c6dd73ecbae61d68b
SerovMikhail <serov.mikhail.job@gmail.com>
2019-09-08 17:29:55 +0200

    find out what takeUntil is

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
-import { from, Subject } from 'rxjs';
-import { multicast } from 'rxjs/operators';
- 
-const source = from([1, 2, 3]);
-const subject = new Subject();
-const multicasted = source.pipe(multicast(subject));
- 
-// These are, under the hood, `subject.subscribe({...})`:
-multicasted.subscribe({
-  next: (v) => console.log(`observerA: ${v}`)
-});
-multicasted.subscribe({
-  next: (v) => console.log(`observerB: ${v}`)
-});
- 
-// This is, under the hood, `source.subscribe(subject)`:
-// multicasted;
-source.subscribe(subject);
+import { ajax } from "rxjs/ajax";
+import { fromEvent, pipe, forkJoin, of, Subject } from "rxjs";
+import {
+  mergeMap,
+  combineAll,
+  concatMap,
+  switchMap,
+  takeUntil
+} from "rxjs/operators";
+
+const API_URL = `https://medicalreachapidev.azurewebsites.net`;
+
+const getCountries = ajax.getJSON(`${API_URL}/api/info/country`);
+const getAllLists = ajax.getJSON(`${API_URL}//\api/info/all-lists`);
+
+const unsubscribe = new Subject();
+
+ajax
+  .getJSON(`${API_URL}/api/info/nationality`)
+  .pipe(takeUntil(unsubscribe))
+  .subscribe(nationality => {
+    console.log("nationality:", nationality);
+  });
+
+// forkJoin({ lists: getAllLists, conuntries: getCountries })
+//   .pipe(
+//     concatMap(mergeData => {
+//       console.log("mergeData:", mergeData);
+//       return of{
+//         mergeData: mergeData,
+//         nationality: ajax.getJSON(`${API_URL}/api/info/nationality`)
+//       };
+//     })
+//   )
+//   .subscribe(_ => {
+//     console.log(_);
+//   });
